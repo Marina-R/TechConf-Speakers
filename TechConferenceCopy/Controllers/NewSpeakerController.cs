75e96863c242db4ce47b6da6bffb0c0067fa1473
Maryna Romaniuk <luv_this_world@hotmail.com>
2015-09-04 22:59:46 +0300

    the form for creating new speaker added

---
+++ TechConferenceCopy/Controllers/NewSpeakerController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TechConferenceCopy.Domain.Context;
+using TechConferenceCopy.Domain.Entities;
+
+
+namespace TechConferenceCopy.Controllers
+{
+    public class NewSpeakerController : Controller
+    {
+        //
+        // GET: /NewSpeaker/
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(Speaker speaker)
+        {
+            if (ModelState.IsValid)
+            {
+                using (TechContext db = new TechContext())
+                {
+                    db.Speaker.Add(speaker);
+                    db.SaveChanges();
+                    ModelState.Clear();
+                    speaker = null;
+                    ViewBag.Message = "Successfully Registration Done";
+                    TempData["Success"] = "Success message text.";
+                    return RedirectToAction("Index", "Home");
+                }
+            } else
+            return View(speaker);
+            //return RedirectToAction("Index", "Home");
+        }
+	}
+}
